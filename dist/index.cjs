c047dfc66ebaeb186234e8f2c9a3809d4b0ef808
The Brigade <wintrcat.wilson@gmail.com>
2025-12-27 01:49:33 +0000

    chore: remove dist folder

--- dist/index.cjs
+++
@@ -1,1 +0,0 @@
-"use strict";const s=require("chessops"),o=s.Chess.default();for(const e of o.dests(s.parseSquare("a2")))console.log(`from: a2 - can go to: ${s.makeSquare(e)}`);
--- dist/index.js
+++
@@ -1,4 +0,0 @@
-import { Chess as s, parseSquare as e, makeSquare as a } from "chessops";
-const r = s.default();
-for (const o of r.dests(e("a2")))
-  console.log(`from: a2 - can go to: ${a(o)}`);
